42c776a5f7209c85cfeea10ffa5b90a33c2c3995
CrystalAice <aicecrystal9@gmail.com>
2026-04-07 17:11:31 +0200

    data analysis.py

--- data analysis.py
+++ data analysis.py
@@ -6,14 +6,14 @@ import seaborn as sns
 #create a sample of 5000 invoices
 def sample():
     retail = pd.read_excel("Online Retail.xlsx")
-    retail = retail.head(5000)
+    retail = retail
     return retail
 
 
 #removing null entries
 def delete():
-    retail = sample()
-    retail = retail.dropna()
+    retail = sample()  
+    retail = retail.dropna(subset=["Quantity", "UnitPrice", "Country"])
     retail = retail.drop_duplicates()
     return retail
 
@@ -25,16 +25,21 @@ def remove():
 
 #creating revenue column
 def specific():
-    retail_revenue = remove()
-    retail_revenue['Revenue'] = retail_revenue['Quantity'] * retail_revenue['UnitPrice']
-    return retail_revenue
+    retail = remove()
+    retail["Revenue"] = retail["Quantity"] * retail["UnitPrice"]
+    revenue_per_country = retail.groupby('Country')['Revenue'].sum()
+    #sampling the top 10 rated countries as per revenue
+    revenue_per_country = revenue_per_country.sort_values(ascending=False).head(10)
+    return revenue_per_country
 
 #plotting a revenue graph
 
-retail_data = specific()
-retail_table = retail_data[['Country', 'Revenue']]
-retail_graph = sns.barplot(x = 'Country', y = 'Revenue', data = retail_table)
-plt.title('Revenue vs Countries')
-plt.show()
-
 print(specific())
+revenue_per_country = specific()
+plt.figure(figsize=(7, 14))
+revenue_plot_data = pd.DataFrame(revenue_per_country)
+revenue_plot_graph = sns.barplot(x = 'Country', y = 'Revenue', data = revenue_plot_data)
+plt.xticks(rotation=90)
+plt.title("Revenue per Country (Top 10 Countries)")
+plt.xlabel("Country")
+plt.ylabel("Revenue")
